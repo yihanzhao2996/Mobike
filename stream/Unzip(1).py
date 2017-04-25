09fb94b6b317d17162b9dd5a6afbf2ecbecb2250
llleeeooo0810 <llleeeooo0810@gmail.com>
2017-04-25 00:45:21 -0400

    Regularize the format of output table

--- stream/Unzip(1).py
+++ stream/Unzip(1).py
@@ -27,7 +27,7 @@ print 'Connection address:', addr
 def unzip(file_name):
     os.system("gunzip -k " + file_name)  # unzip type: -k keep the origin .gz file
     csv_name = file_name[:-3]
-    data = list(csv.reader(open(csv_name, 'rb'), delimiter=','))
+    data = list(csv.reader(open(csv_name, 'r'), delimiter=','))
     for row in data:
         del row[3:6]  # keep 5 columns: timeStamp, bikeID(end with '#'), bikeType, longitude, latitude
     gatherByTime(data)
@@ -35,42 +35,18 @@ def unzip(file_name):
 
 def gatherByTime(input_data):
     basetime = input_data[0][0]
-    # print basetime
     start = 0
     end = 0
-    # sc = SparkContext(appName="PythonStreamingQueueStream")
-    # ssc = StreamingContext(sc, 1)
-    # rddQueue = []
-    # zkQuorum, topic = sys.argv[1:]
     for row in input_data:
         if row[0] == basetime:
             end += 1
-        else:  # when detect the change in timeStamp, dump the file as pickle and send it
-            # pickle.dump(input_data[start:end+1], open('%s.pkl' % basetime, 'wb'))
+        else:
             conn.send(pickle.dumps(input_data[start:end+1]))
-            # rddQueue += [ssc.sparkContext.parallelize(input_data[start:end+1],10)]
-            # start = end + 1
-            # end += 1
-            # basetime = row[0]
             time.sleep(1)
             if end == len(input_data):
-                # rddQueue += [ssc.sparkContext.parallelize(input_data[-1],10)]
                 conn.send(pickle.dumps(input_data[-1]))
-                # pickle.dump(input_data[-1], open(basetime, 'wb'))
     conn.close()
-    # inputStream = ssc.queueStream(rddQueue)
-    # kafka_stream = KafkaUtils.createStream(ssc, \
-    #     zkQuorum, \
-    #     "spark-streaming-consumer", \
-    #     {topic: 1})
-
-    # ssc.start()
-    # ssc.awaitTermination()
-
 
 
 if __name__ == '__main__':
-    # if len(sys.argv) != 3:
-    #     print("Usage: Unzip.py <zk> <topic>")
-    #     exit(-1)
     data = unzip('20170412-200908.csv.gz')
--- stream/lis.py
+++ stream/lis.py
@@ -7,8 +7,10 @@ from pyspark.streaming import StreamingContext
 from threading import Thread
 import time
 import datetime
+from geopy.distance import great_circle
 
-def listener(sc,ssc):
+
+def listener(sc, ssc):
 	TCP_IP = '127.0.0.1'
 	TCP_PORT = 9999
 	BUFFER_SIZE = 40960
@@ -33,34 +35,36 @@ def main():
 	# start listener
 	thread1 = Thread(target=listener, args=(sc,ssc))
 	thread1.start()
-	# while True:
-	# 	print rddQueue
-		# pass
+
 	time.sleep(5)
 	inputStream = ssc.queueStream(rddQueue)
 	bike_table = ssc.queueStream([ssc.sparkContext.parallelize((),10)])
+	inputStream.pprint()
 	mappedStream0 = inputStream.map(lambda x: (x[1],(x[0],x[2],x[3],x[4]))).groupByKey()
 	def f(x): return list(x)[0]
 	mappedStream = mappedStream0.mapValues(f)
-	# deduplicate
-	# mappedStream.cogroup()
-	# pair
-	# common_table = bike_table.join(mappedStream)
+	# mappedStream.pprint()
+	# mappedStream using BikeID as key, including time, type, longitude, latitude
+	# the new received data stored in mappedStream
+
 	common_table = mappedStream.join(mappedStream)
-	update_rdd = common_table.map(lambda x : (x[0], \
-		(datetime.datetime.strptime(x[1][0][0], "%Y-%m-%dT%H:%M:%S")-datetime.datetime.strptime(x[1][1][0], "%Y-%m-%dT%H:%M:%S"), \
-		(float(x[1][0][2])- float(x[1][1][2]))**2 + (float(x[1][0][3]) - float(x[1][1][3]))**2))) \
-		.filter(lambda x: x[0] == '0280039122#')
-	update_rdd.pprint()
-	mappedStream.pprint()
+	# select only the pieces of data having the same BikeId
+	update_rdd = common_table.map(lambda x : (x[0], x[1][0][0], x[1][1][0],
+											  (datetime.datetime.strptime(x[1][0][0], "%Y-%m-%dT%H:%M:%S")-
+											  datetime.datetime.strptime(x[1][1][0], "%Y-%m-%dT%H:%M:%S")).total_seconds(),
+											  (float(x[1][0][2]), float(x[1][0][3])),
+											  (float(x[1][1][2]), float(x[1][1][3])),
+											  great_circle((float(x[1][0][2]), float(x[1][0][3])), (float(x[1][1][2]), float(x[1][1][3]))).miles))\
+		.filter(lambda x: x[3] != .0)
+	# updated_bike = update_rdd.sortByKey()
+	# update_rdd.pprint()
+	# mappedStream.pprint()
 	bike_table = mappedStream.leftOuterJoin(bike_table)
-	bike_table.pprint()
-	# new_bike_table = mappedStream.transform(lambda rdd: rdd.leftOuterJoin(bike_table))
-	# new_bike_table.pprint()
-	# nn = new_bike_table.leftOuterJoin(mappedStream)
-	# nn.pprint()
+	# bike_table.pprint()
+
 	ssc.start()
-	# thread1.join()
+	ssc.awaitTermination()
+
 
 if __name__ == '__main__':
 	main()
